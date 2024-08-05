09ee9e403cc5b04c91586e9e3d494a14385a5e50
Pedro William <pewilliam.ac@outlook.com>
2024-08-05 19:16:30 -0300

    Add project files.

---
+++ Teste_Software_2024_Carvalho_Pedro/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Teste_Software_2024_Carvalho_Pedro")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Teste_Software_2024_Carvalho_Pedro")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("575e00f9-9bca-4cf4-8da2-3d341a52ee54")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Teste_Software_2024_Carvalho_Pedro/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Threading.Tasks;
+
+namespace Teste_Software_2024_Carvalho_Pedro
+{
+    public interface ISomeInterface
+    {
+        Task DoSomethingAsync();
+    }
+
+    [TestClass]
+    public class MyTests
+    {
+        [TestMethod]
+        public async Task MyAsyncTest()
+        {
+            var mock = new Mock<ISomeInterface>();
+
+            // Incorrect setup
+            //mock.Setup(arg => arg.DoSomethingAsync()).Callback(() => { /* some code */ });
+
+            // Correct setup
+            mock.Setup(arg => arg.DoSomethingAsync()).Returns(Task.CompletedTask);
+
+            // Execute code
+            await mock.Object.DoSomethingAsync();
+        }
+    }
+}
